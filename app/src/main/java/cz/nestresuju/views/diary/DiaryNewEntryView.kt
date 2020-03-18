0ac4907e1d502f2d0d5d7f31687896c62a3ccd04
Ondřej John <ondrej.john@gmail.com>
2020-03-18 19:02:23 +0100

    🎨 Extract common question view parts to separate view

--- app/src/main/java/cz/nestresuju/views/diary/DiaryNewEntryView.kt
+++ app/src/main/java/cz/nestresuju/views/diary/DiaryNewEntryView.kt
@@ -3,11 +3,6 @@ package cz.nestresuju.views.diary
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.cardview.widget.CardView
 import cz.nestresuju.R
 import cz.nestresuju.common.extensions.visible
@@ -23,10 +18,7 @@ class DiaryNewEntryView(context: Context, attributes: AttributeSet) : CardView(c
     private val smiley3: DiaryStressLevelView
     private val smiley4: DiaryStressLevelView
     private val btnNote: DiaryStressLevelView?
-    private val layoutAnswer: ViewGroup
-    private val editAnswer: EditText
-    private val btnConfirm: ImageView
-    private val txtQuestion: TextView
+    private val questionView: DiaryQuestionView
 
     init {
         val attrs = context.obtainStyledAttributes(attributes, R.styleable.DiaryNewEntryView)
@@ -40,25 +32,14 @@ class DiaryNewEntryView(context: Context, attributes: AttributeSet) : CardView(c
         smiley2 = findViewById(R.id.smiley_2)
         smiley3 = findViewById(R.id.smiley_3)
         smiley4 = findViewById(R.id.smiley_4)
-        layoutAnswer = findViewById(R.id.layout_answer)
-        editAnswer = findViewById(R.id.edit_answer)
-        btnConfirm = findViewById(R.id.btn_confirm)
-        txtQuestion = findViewById(R.id.txt_question)
         btnNote = if (mode == 1) findViewById(R.id.input_note) else null
+        questionView = findViewById(R.id.view_question)
 
         attrs.recycle()
-
-        editAnswer.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                btnConfirm.performClick()
-                return@setOnEditorActionListener true
-            }
-            return@setOnEditorActionListener false
-        }
     }
 
     fun setOnConfirmationListener(listener: (String) -> Unit) {
-        btnConfirm.setOnClickListener { listener(editAnswer.text.toString()) }
+        questionView.setOnConfirmationListener(listener)
     }
 
     fun setOnStressLevelSelectedListener(listener: (StressLevelInput) -> Unit) {
@@ -76,11 +57,13 @@ class DiaryNewEntryView(context: Context, attributes: AttributeSet) : CardView(c
         smiley4.setOptionSelected(stressLevel == StressLevelInput.STRESSED)
         btnNote?.setOptionSelected(stressLevel == StressLevelInput.NONE)
 
-        layoutAnswer.visible = true
-        txtQuestion.text = question
+        with(questionView) {
+            visible = true
+            setQuestion(question)
+        }
     }
 
     fun clearAnswer() {
-        editAnswer.text.clear()
+        questionView.clearAnswer()
     }
 }
---
+++ app/src/main/java/cz/nestresuju/views/diary/DiaryQuestionView.kt
@@ -0,0 +1,42 @@
+package cz.nestresuju.views.diary
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.inputmethod.EditorInfo
+import androidx.constraintlayout.widget.ConstraintLayout
+import cz.nestresuju.databinding.ViewDiaryInputQuestionBinding
+
+/**
+ * Input view for stress level question.
+ */
+class DiaryQuestionView(context: Context, attributes: AttributeSet) : ConstraintLayout(context, attributes) {
+
+    private val binding = ViewDiaryInputQuestionBinding.inflate(LayoutInflater.from(context), this, true)
+
+    init {
+        with(binding) {
+            editAnswer.setOnEditorActionListener { _, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    btnConfirm.performClick()
+                    return@setOnEditorActionListener true
+                }
+                return@setOnEditorActionListener false
+            }
+        }
+    }
+
+    fun setOnConfirmationListener(listener: (String) -> Unit) {
+        with(binding) {
+            btnConfirm.setOnClickListener { listener(editAnswer.text.toString()) }
+        }
+    }
+
+    fun setQuestion(question: String) {
+        binding.txtQuestion.text = question
+    }
+
+    fun clearAnswer() {
+        binding.editAnswer.text.clear()
+    }
+}
