0b677e03e2631cb7f1ec63d29292efa5fd113d86
Ondřej John <ondrej.john@gmail.com>
2020-04-01 21:25:03 +0200

    ✨ Prepare UI for screening test

--- app/src/main/java/cz/nestresuju/common/extensions/ContextExtensions.kt
+++ app/src/main/java/cz/nestresuju/common/extensions/ContextExtensions.kt
@@ -2,6 +2,8 @@ package cz.nestresuju.common.extensions
 
 import android.content.Context
 import android.content.Intent
+import android.util.TypedValue
+import androidx.fragment.app.Fragment
 
 /**
  * Extensions for common tasks related to Android [Context].
@@ -11,3 +13,11 @@ fun Context.startActivity(activityClass: Class<*>) {
     val intent = Intent(this, activityClass)
     startActivity(intent)
 }
+
+fun Context.dp(dp: Float) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics).toInt()
+
+fun Context.dp(dp: Int) = dp(dp.toFloat())
+
+fun Fragment.dp(dp: Float) = requireContext().dp(dp)
+
+fun Fragment.dp(dp: Int) = requireContext().dp(dp)
--- app/src/main/java/cz/nestresuju/screens/tests/screening/ScreeningTestFragment.kt
+++ app/src/main/java/cz/nestresuju/screens/tests/screening/ScreeningTestFragment.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import cz.nestresuju.common.extensions.dp
 import cz.nestresuju.databinding.FragmentScreeningTestBinding
 import cz.nestresuju.screens.base.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -13,9 +18,33 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
  */
 class ScreeningTestFragment : BaseFragment<FragmentScreeningTestBinding>() {
 
+    // TODO: remove UI mockup data and implement logic
+
     override val viewModel by viewModel<ScreeningTestViewModel>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return FragmentScreeningTestBinding.inflate(inflater, container, false).also { _binding = it }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(viewBinding) {
+            layoutAnswers.removeAllViews()
+            layoutAnswers.addAnswerCheckbox("Stěhování")
+            layoutAnswers.addAnswerCheckbox("Studium")
+            layoutAnswers.addAnswerCheckbox("Práce")
+        }
+    }
+
+    private fun LinearLayout.addAnswerCheckbox(answerText: String) {
+        val checkBox = CheckBox(requireContext()).apply {
+            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
+                topMargin = dp(4)
+                bottomMargin = dp(4)
+            }
+            text = answerText
+        }
+
+        addView(checkBox)
     }
 }
