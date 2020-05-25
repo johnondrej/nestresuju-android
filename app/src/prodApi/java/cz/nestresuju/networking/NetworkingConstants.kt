ab1c592e194128dd0dd7a7a9866083fd8555b43f
Ondřej John <ondrej.john@gmail.com>
2020-05-25 23:16:09 +0200

    🐛 Fix auth configuration for production API

--- app/src/prodApi/java/cz/nestresuju/networking/NetworkingConstants.kt
+++ app/src/prodApi/java/cz/nestresuju/networking/NetworkingConstants.kt
@@ -8,8 +8,8 @@ object NetworkingConstants {
     const val API_BASE_URL = "https://api.nestresuju.cz/"
     const val API_AUTH_BASE_URL = "https://auth.nestresuju.cz/"
 
-    const val AUTH_CLIENT_ID = "client.mobile"
-    const val AUTH_CLIENT_SECRET = "mobile_secret"
+    const val AUTH_CLIENT_ID = "client.android"
+    const val AUTH_CLIENT_SECRET = "android_secret"
     const val AUTH_GRANT_TYPE_PASSWORD = "password"
     const val AUTH_GRANT_TYPE_REFRESH_TOKEN = "refresh_token"
     const val AUTH_SCOPE = "nestresuju_mobile offline_access"
